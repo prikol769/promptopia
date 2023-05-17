025180ab14eb2330fe1b4ff57548f396ac56e500
prikol769 <prikol769@gmail.com>
2023-05-17 17:07:39 -0500

    create Home page and auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
     title: "Promptopia",
@@ -7,15 +9,18 @@ export const metadata = {
 const RootLayout = ({children}) => {
     return (
         <html lang="en">
-            <body>
-                <div className="main">
-                    <div className="gradient" />
-                </div>
+        <body>
+        <Provider>
+            <div className="main">
+                <div className="gradient"/>
+            </div>
 
             <main className="app">
+                <Nav/>
                 {children}
             </main>
-            </body>
+        </Provider>
+        </body>
         </html>
     );
 };
