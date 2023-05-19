31f7c32c3d0313857074600fcb16223cbefa3c94
prikol769 <prikol769@gmail.com>
2023-05-19 13:33:21 -0500

    added functionality Edit and Delete Posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,9 +4,11 @@ import {useSession} from 'next-auth/react';
 import {useRouter} from 'next/navigation';
 
 import Profile from '@components/Profile';
+import {router} from '@node_modules/next/dist/client';
 
 const MyProfile = () => {
     const {data: session} = useSession();
+    const router = useRouter();
 
     const [posts, setPosts] = useState([]);
 
@@ -20,12 +22,25 @@ const MyProfile = () => {
        if(session?.user.id) fetchPosts();
     }, [])
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`);
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+        if(hasConfirmed) {
+            try{
+                await fetch(`api/prompt/${post._id.toString()}`, {
+                    method: 'DELETE'
+                });
 
+                const filteredPost = posts.filter((p) => p._id !== post._id);
+
+                setPosts(filteredPost);
+            }catch (error){
+                console.log(error);
+            }
+        }
     }
     return (
         <Profile
